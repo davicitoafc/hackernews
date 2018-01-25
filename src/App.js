f02c6e51848875bf07c4ee53b801d9da9de647bf
David Cisneros <d.cisneros849@gmail.com>
2018-01-25 16:39:50 -0500

    yo

--- src/App.js
+++ src/App.js
@@ -22,14 +22,21 @@ const list = [
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-       {list.map(function(item) {
-          return <div>{item.title}</div>
-       })}
+     return (
+     <div className="App">
+       {list.map(item =>
+            <div key={item.objectID}>
+              <span>
+               <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+             </div>
+          )}
       </div>
-    );
-  }
+  )
+ }
 }
 
 export default App;
